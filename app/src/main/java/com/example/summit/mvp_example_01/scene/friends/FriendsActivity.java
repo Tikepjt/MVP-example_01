6d9e4965ab4f735a5e595895ec8e0a091d25d191
tikepjt <tikepjt@gmail.com>
2018-08-07 18:14:45 +0900

    first commit

---
+++ app/src/main/java/com/example/summit/mvp_example_01/scene/friends/FriendsActivity.java
@@ -0,0 +1,15 @@
+package com.example.summit.mvp_example_01.scene.friends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.summit.mvp_example_01.R;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+}
